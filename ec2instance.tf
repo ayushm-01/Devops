eae2b26eaff45beff5588eb2f264ea59441728a0
ayushm-01 <92621603+ayushm-01@users.noreply.github.com>
2024-04-12 11:19:23 +0530

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"  # Update with your desired AWS region
 }
 
-resource "aws_instance" "example" {
+resource "devoops" "example" {
   ami           = "ami-080e1f13689e07408"  # Update with your desired AMI ID
   instance_type = "t2.micro"
   tags = {
